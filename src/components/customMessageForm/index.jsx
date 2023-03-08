5eb1b886e94ff699fce2d7d3459632e4be739fbb
Mario Bermudez JR <mario.bermudez.jr@outlook.com>
2023-03-08 19:34:01 +0700

    Add ability to submit messages and images using the native input property

--- src/components/customMessageForm/index.jsx
+++ src/components/customMessageForm/index.jsx
@@ -1,7 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import Dropzone from 'react-dropzone'
 
-const customMessageForm = () => {
-  return <div>customMessageForm</div>
+const StandardMessageForm = ({ props, activeChat, appendText, handleKeyDown }) => {
+  const [message, setMessage] = useState('')
+  const [attachment, setAttachment] = useState('')
+  const [preview, setPreview] = useState('')
+
+  const handleChange = (e) => setMessage(e.target.value)
+  const handleSubmit = async () => {
+    const date = new Date()
+      .toISOString()
+      .replace('T', ' ')
+      .replace('Z', `${Math.floor(Math.random() * 1000)}+00:00`)
+    const at = attachment ? [{ blob: attachment, file: attachment.name }] : []
+    const form = {
+      attachments: at,
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    }
+
+    props.onSubmit(form)
+    setMessage('')
+    setAttachment('')
+  }
+
+  return (
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img
+            alt="message-form-preview"
+            className="message-form-preview-image"
+            src={preview}
+            onLoad={() => URL.revokeObjectURL(preview)}
+          />
+          <XMarkIcon
+            className="message-form-icon-x"
+            onClick={() => {
+              setPreview('')
+              setAttachment('')
+            }}
+          />
+        </div>
+      )}
+      <div className="message-form">
+        <div className="message-form-input-container">
+          <input
+            className="message-form-input"
+            type="text"
+            value={message}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            placeholder="Send a message..."
+          />
+          {appendText && (
+            <input
+              className="message-form-assist"
+              type="text"
+              disabled="disabled"
+              value={`${message} ${appendText}`}
+            />
+          )}
+        </div>
+        <div className="message-form-icons">
+          <Dropzone
+            acceptedFiles=".jpg,.jpeg,.png"
+            multiple={false}
+            noClick={true}
+            onDrop={(acceptedFiles) => {
+              setAttachment(acceptedFiles[0])
+              setPreview(URL.createObjectURL(acceptedFiles[0]))
+            }}
+          >
+            {({ getRootProps, getInputProps, open }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <PaperClipIcon className="message-form-icon-clip" onClick={open} />
+              </div>
+            )}
+          </Dropzone>
+
+          <hr className="vertical-line" />
+          <PaperAirplaneIcon
+            className="message-form-icon-airplane"
+            onClick={() => {
+              setPreview('')
+              handleSubmit()
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default customMessageForm
+export default StandardMessageForm
